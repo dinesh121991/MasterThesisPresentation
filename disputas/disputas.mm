57429581b257d58fac10388ab7aec6b540f54a82
dinesh <dinesh121991@gmail.com>
2015-09-14 06:30:09 +0530

    Final Presentation Images and Slides

---
+++ disputas/disputas.mm
@@ -0,0 +1,195 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1260451443379" ID="ID_1917486432" MODIFIED="1260781415076" STYLE="bubble" TEXT="Disputas">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1260781374531" ID="ID_823331775" MODIFIED="1260781415076" POSITION="right" TEXT="What are we trying to do?">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1260520964452" ID="ID_987295155" MODIFIED="1260520993453" TEXT="Goal: Study motion of electrons and atoms">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1260781297821" ID="ID_538740296" MODIFIED="1260781311339" TEXT="Calculations must be performed on a computer">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1260781313975" ID="ID_1538530147" MODIFIED="1260781330612" TEXT="Equations of motion must be cast in a form sutiable for computation">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1260781391284" ID="ID_1503557636" MODIFIED="1260781415077" TEXT="&quot;Solving&quot; equations of motion">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1260781358018" ID="ID_280647280" MODIFIED="1260781371967" TEXT="Computing Classical Mechanics">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1260451443379" ID="ID_218266007" MODIFIED="1260529411560" POSITION="right" STYLE="bubble" TEXT="Introduction to QM">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1260451443379" ID="ID_450169614" MODIFIED="1260529411587" TEXT="Why not classical mechanics?">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1260521026519" ID="ID_309072305" MODIFIED="1260529411642" TEXT="Completely different scales than our ordinary lives">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1260521061933" ID="ID_1250713036" MODIFIED="1260521206947" TEXT="Length scale ~0.1nm (perhaps show scale video)">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1260521102860" ID="ID_740150152" MODIFIED="1260529325009">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Amazingly fast timescales (1&#181;s is an eternity)
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1260451443379" ID="ID_1105001499" MODIFIED="1260529411588" TEXT="What is QM">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1260451443379" ID="ID_1597438496" MODIFIED="1260451560904" TEXT="Probabilistic approach">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1260451443379" ID="ID_586730302" MODIFIED="1260451560906" TEXT="Schr&#xf6;dinger equation">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1260451443379" ID="ID_1600794678" MODIFIED="1260529411590" TEXT="What makes QM difficult">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1260451443379" ID="ID_1399753746" MODIFIED="1260451560907" TEXT="compared to CM">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1260521264873" ID="ID_803007757" MODIFIED="1260529411644" TEXT="Must take into account every &#xa;possibility for where a particle can &#xa;be and which speed it might have">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1260521452570" ID="ID_413475408" MODIFIED="1260521514539">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      This leads to very large problems, which in
+    </p>
+    <p>
+      turn requires very large computers to be solved
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1260451443379" ID="ID_1537185024" MODIFIED="1260529411562" POSITION="right" STYLE="bubble" TEXT="Interesting Problems">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1260451443379" ID="ID_1136312976" MODIFIED="1260529411591" TEXT="Laser Ionization of Helium">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1260451592526" ID="ID_1384367933" MODIFIED="1260452549562" TEXT="Spherical 3D">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1260451597822" ID="ID_314932108" MODIFIED="1260452549562" TEXT="Two electrons =&gt; 6D">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1260451661008" ID="ID_1185642208" MODIFIED="1260521431450" TEXT="Symmetries =&gt; 5D">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1260451443379" ID="ID_1399505599" MODIFIED="1260529411592" TEXT="Ionic collision with H2">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1260451606415" ID="ID_1093467622" MODIFIED="1260452549564" TEXT="Cartesian">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1260451617265" ID="ID_886516264" MODIFIED="1260452549564" TEXT="One electron">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1260451620473" ID="ID_655027125" MODIFIED="1260452549565" TEXT="Very large grid">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1260451443379" ID="ID_1454317231" MODIFIED="1260529411592" TEXT="Quantum Dots">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1260451640677" ID="ID_1716122191" MODIFIED="1260452549566" TEXT="Cartesian 2D">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1260451646445" ID="ID_522156641" MODIFIED="1260452549566" TEXT="Two electrons =&gt; 4D">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1260451443380" ID="ID_86566086" MODIFIED="1260529411564" POSITION="right" STYLE="bubble" TEXT="Common Denominators">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1260451443380" ID="ID_971051977" MODIFIED="1260529411593" TEXT="Simple (or at least regular) Geometries">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1260451443380" ID="ID_1346994444" MODIFIED="1260451560911" TEXT="Spherical or Cartesian">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1260451443380" ID="ID_602149146" MODIFIED="1260451560912" TEXT="Could also support Cylindrical or similar coordinate system">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1260451729987" ID="ID_1678201404" MODIFIED="1260451749417" TEXT="Separable into tensor product of independent ranks">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1260451443380" ID="ID_651257904" MODIFIED="1260529411565" POSITION="right" STYLE="bubble" TEXT="Exploiting similarities with PyProp">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1260452691552" ID="ID_450363967" MODIFIED="1260529411594" TEXT="Separate time and space discretization">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1260452709027" ID="ID_1475095557" MODIFIED="1260452719424" TEXT="Time is an IVP">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1260452719844" ID="ID_1875328404" MODIFIED="1260529411646" TEXT="Space is a BVP">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1260452750494" HGAP="27" ID="ID_1535611236" MODIFIED="1260452804373" TEXT="march each space point in time" VSHIFT="-10"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1260451443380" ID="ID_1401610112" MODIFIED="1260529411594" TEXT="Separate problem in ranks">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1260451443380" ID="ID_39857015" MODIFIED="1260451560913" TEXT="each rank should be relatively small">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1260451443380" ID="ID_109590244" MODIFIED="1260451560914" TEXT="the total system is the outer product of the ranks">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1260451443380" ID="ID_430241342" MODIFIED="1260451939647" TEXT="Each rank can be discretised separately">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1260454815346" ID="ID_805326362" MODIFIED="1260529411595" TEXT="Common tools for any combination of ranks">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1260520860574" ID="ID_256719031" MODIFIED="1260529411646" TEXT="Wavefunction">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1260454839605" ID="ID_1677750883" MODIFIED="1260520866345" TEXT="Inner Product">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node COLOR="#111111" CREATED="1260520871035" ID="ID_1941926603" MODIFIED="1260520873607" TEXT="Redistribution"/>
+</node>
+<node COLOR="#990000" CREATED="1260454848903" ID="ID_769762685" MODIFIED="1260454862325" TEXT="Advance Timestep">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1260454852607" ID="ID_1658098656" MODIFIED="1260454856308" TEXT="Matrix-Vector product">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+</node>
+</map>
